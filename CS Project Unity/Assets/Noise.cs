3afe8130de45028b575d8b80bbdf201a35bdcdea
SamDower <samdower@hotmail.co.uk>
2020-06-09 17:07:00 +0100

    Basic noise. First marching cubes generation. Simple Gizmos. Chunk based.

---
+++ CS Project Unity/Assets/Noise.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Noise 
+{
+    public static float Perlin3D(float x, float y, float z, Vector3 offset, float scale)
+    {
+        float ab = Mathf.PerlinNoise((x + offset.x) / scale, (y + offset.y) / scale);
+        float bc = Mathf.PerlinNoise((y + offset.y) / scale, (z + offset.z) / scale);
+        float ac = Mathf.PerlinNoise((x + offset.x) / scale, (z + offset.z) / scale);
+
+        float ba = Mathf.PerlinNoise((y + offset.y) / scale, (x + offset.x) / scale);
+        float cb = Mathf.PerlinNoise((z + offset.z) / scale, (y + offset.y) / scale);
+        float ca = Mathf.PerlinNoise((z + offset.z) / scale, (x + offset.x) / scale);
+
+        float abc = ab + bc + ac + ba + cb + ca;
+        return abc / 6f;
+    }
+}
